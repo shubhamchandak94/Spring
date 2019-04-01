227a3e51c14d79e1bb4468dede98e035cb52f9c1
Shubham Chandak <schandak@stanford.edu>
2019-04-01 16:28:01 -0700

    add license

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,3 +1,14 @@
+/*
+* Copyright 2018 University of Illinois Board of Trustees and Stanford University. All Rights Reserved.
+* Licensed under the “Non-exclusive Research Use License for SPRING Software” license (the "License");
+* You may not use this file except in compliance with the License.
+* The License is included in the distribution as license.pdf file.
+ 
+* Software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and limitations under the License.
+*/
+
 #include "encoder.h"
 #include <omp.h>
 #include <algorithm>
